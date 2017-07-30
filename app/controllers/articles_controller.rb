dbfecd8f3d914dad7e41c2d1a1968acc0bb53820
Todd Wolfson <todd@twolfson.com>
2017-07-29 22:04:36 -0700

    Added article creation logic, so much magic in Rails :sparkles:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render(:plain => params[:article].inspect())
+    # Create and save our article
+    @article = Article.new(article_params())
+    @article.save()
+
+    # Redirect to our article's page to avoid double submissions
+    redirect_to(@article)
   end
+
+  private
+    def article_params
+      return params.require(:article).permit(:title, :text)
+    end
 end

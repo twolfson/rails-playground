4aa3c95e1289504dedd1490df8a59f2795b2c22b
Todd Wolfson <todd@twolfson.com>
2017-11-01 00:59:31 -0700

    Completed article saving

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render(:plain => params[:article].inspect())
+    # Create our article
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    # Save our article
+    # TODO: What's the difference between `save()` and `save!()`?
+    @article.save()
+
+    # Redirect to our article's page
+    # TODO: What's the magic method for the article's URL and what does the magic in `redirect_to` look like?
+    return redirect_to(@article)
   end
 end

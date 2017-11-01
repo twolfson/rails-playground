d94eb6502b6cb8d6f8830df7fe23e80a5b5e7f94
Todd Wolfson <todd@twolfson.com>
2017-11-01 00:45:28 -0700

    Added articles resources

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+class ArticlesController < ApplicationController
+  def new
+    # Do nothing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root('welcome#index')
 
+  # Define our articles resources
+  resources(:articles)
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

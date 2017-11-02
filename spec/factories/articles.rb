adcca74a75b77668f59a7d6cd40e3eac2039003e
Todd Wolfson <todd@twolfson.com>
2017-11-02 02:47:04 -0700

    Fixed up article quirks

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,10 +2,10 @@ FactoryGirl.define() do
   factory(:article) do
     title("First title")
     text("First text")
-  end
 
-  factory(:article2) do
-    title("Second title")
-    text("Second text")
+    factory(:article2) do
+      title("Second title")
+      text("Second text")
+    end
   end
 end
--- spec/requests/get_articles_spec.rb
+++ spec/requests/get_articles_spec.rb
@@ -11,8 +11,15 @@ RSpec.describe("GET /articles", :type => :request) do
 
   describe("with articles") do
     it("lists article content") do
+      # Set up our fixtures
+      create(:article)
+      create(:article2)
+
+      # Make our request
       get(articles_path)
       expect(response).to(have_http_status(200))
+
+      # Verify our content
       expect(response.body).to(include("foo"))
     end
   end

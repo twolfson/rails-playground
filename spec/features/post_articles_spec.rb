7ea29190efa8b2cdcd45f8eac8b29009b5b0bbba
Todd Wolfson <todd@twolfson.com>
2017-11-03 02:08:17 -0700

    Started on Capybara feature tests

--- spec/features/post_articles_spec.rb
+++ spec/features/post_articles_spec.rb
@@ -2,19 +2,23 @@
 require("rails_helper")
 
 # Start our tests
-RSpec.describe("POST /articles", :type => :request) do
+RSpec.describe("POST /articles", :type => :feature) do
   # non_existent: N/A, non_owner: N/A, logged_out: N/A
   describe("with valid data") do
     before do
       # Load our form-based page
-      # TODO: Consolidate status check into initial load
-      get("/articles/new")
-      expect(response).to have_http_status(200)
+      visit("/articles/new")
 
       # Fill out our form and submit it
+      within("form[action=\"/articles\"][method=post]") do
+        fill_in("Title", :with => "Test title")
+        fill_in("Text", :with => "Test text")
+      end
+      click_button("Create Article")
     end
 
-    it("redirects to articles listing") do
+    skip("redirects to articles listing") do
+      expect(current_url).to(equal("/articles"))
     end
 
     skip("creates a new article in our database") do

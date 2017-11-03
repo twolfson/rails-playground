43befe47d360c7cb75da90a49105433fdc9b256d
Todd Wolfson <todd@twolfson.com>
2017-11-03 02:30:08 -0700

    Starting on invalid form submissions

--- spec/features/post_articles_spec.rb
+++ spec/features/post_articles_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe("POST /articles", :type => :feature) do
 
       # Fill out our form and submit it
       within("form[action=\"/articles\"][method=post]") do
+        # TODO: Replace checking text for field with `name`
         fill_in("Title", :with => "Test title")
         fill_in("Text", :with => "Test text")
       end
@@ -18,7 +19,7 @@ RSpec.describe("POST /articles", :type => :feature) do
     end
 
     it("redirects to article's page") do
-      expect(page.current_path).to(match(/\/articles\/\d+/))
+      expect(page.current_path).to(match(/^\/articles\/\d+$/))
     end
 
     it("creates a new article in our database") do
@@ -30,7 +31,25 @@ RSpec.describe("POST /articles", :type => :feature) do
   end
 
   describe("with invalid data") do
-    skip("re-renders the same page with errors") do
+    before do
+      # Load our form-based page
+      visit("/articles/new")
+
+      # Fill out our form and submit it
+      within("form[action=\"/articles\"][method=post]") do
+        # TODO: Replace checking text for field with `name`
+        fill_in("Title", :with => "foo")
+        fill_in("Text", :with => "Test text")
+      end
+      click_button(:visible => "input[type=submit]")
+    end
+
+    it("re-renders the same page with errors") do
+      # TODO: Is it possible for Rails to submit to the same page?
+      #   I feel like this is REST targeted which is impractical for HTML forms
+      #   i.e. People can't submit and later refresh their page
+      expect(page.current_path).to(match(/^\/articles$/))
+      # TODO: Verify same values are in our form elements
     end
   end
 end

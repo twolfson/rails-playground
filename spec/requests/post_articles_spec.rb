8042913380158636f4180db34e54befa9d9776b2
Todd Wolfson <todd@twolfson.com>
2017-11-03 01:39:09 -0700

    Starting on form submission tests

---
+++ spec/requests/post_articles_spec.rb
@@ -0,0 +1,26 @@
+require("rails_helper")
+
+RSpec.describe("POST /articles", :type => :request) do
+  # non_existent: N/A, non_owner: N/A, logged_out: N/A
+  describe("with valid data") do
+    before do
+      # Load our form-based page
+      # TODO: Consolidate status check into initial load
+      get("/articles/new")
+      expect(response).to have_http_status(200)
+
+      # Fill out our form and submit it
+    end
+
+    it("redirects to articles listing") do
+    end
+
+    skip("creates a new article in our database") do
+    end
+  end
+
+  describe("with invalid data") do
+    skip("re-renders the same page with errors") do
+    end
+  end
+end
